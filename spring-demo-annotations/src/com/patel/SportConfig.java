bf38a37ea74b6653d598a9687c4f10e8a2fb049f
MacBook <patelyogesh5040@gmail.com>
2020-11-05 18:19:12 +0530

    Spring java class config No XML

---
+++ spring-demo-annotations/src/com/patel/SportConfig.java
@@ -0,0 +1,10 @@
+package com.patel;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.patel")
+public class SportConfig {
+
+}
