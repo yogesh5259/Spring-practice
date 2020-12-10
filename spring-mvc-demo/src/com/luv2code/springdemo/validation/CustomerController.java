1eef3dad3b70a5d0a0b2a0a8f32b0b5ceb59c8f1
MacBook <patelyogesh5040@gmail.com>
2020-12-10 17:22:29 +0530

    Spring mvc validation

--- spring-mvc-demo/src/com/luv2code/springdemo/validation/CustomerController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/validation/CustomerController.java
@@ -2,9 +2,12 @@ package com.luv2code.springdemo.validation;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// Add initbinder... to convert trim input strings
+	// Remove leading and trailing whitespace
+	// resolve issue for our validation.
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
 		theModel.addAttribute("customer",new Customer());
