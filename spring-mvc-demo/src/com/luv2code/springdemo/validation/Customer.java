cc9ddfc314b694d71cef4c783185308a44fc2ce9
MacBook <patelyogesh5040@gmail.com>
2020-12-10 17:53:26 +0530

    spring validation

--- spring-mvc-demo/src/com/luv2code/springdemo/validation/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/validation/Customer.java
@@ -14,10 +14,10 @@ public class Customer {
 	@Size(min = 1,message = "is required" )
 	private String lastName;
 	
-	
+	@NotNull(message = "is required!")
 	@Min(value = 0, message = "must be greater than or equal to 0")
 	@Max(value = 10, message = "must be less than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 char/digits")
 	private String postalCode;
@@ -38,11 +38,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
