4482f876f7f33e983bdca092138c0909457231a2
MacBook <patelyogesh5040@gmail.com>
2021-01-21 19:31:46 +0530

    spring projects

--- Spring-mvc-project/web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ Spring-mvc-project/web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,25 +5,28 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-
-	//need to inject the customer dao
+	
+	//need to inject our customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
-		//get the customers from dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		//get the customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		//add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
