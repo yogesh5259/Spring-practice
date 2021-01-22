cdbec336c7a2e6239852e4bacb5fb6a45a2f5043
MacBook <patelyogesh5040@gmail.com>
2021-01-22 19:43:35 +0530

    spring

--- Spring-mvc-project/web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ Spring-mvc-project/web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -33,4 +33,18 @@ public class CustomerController {
 		
 		return "list-customers";
 	}
+	
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		//create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		
+		theModel.addAttribute("customer",theCustomer);
+		
+		
+		return "customer-form";
+	}
+	
 }
